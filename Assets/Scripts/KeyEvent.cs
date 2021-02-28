467fa0cd0b704c073748bd913fa6a181bb3b9952
roosasvn <weiqing@usc.edu>
2021-02-28 05:12:39 -0800

    key and box randomly spawn function

--- Assets/Scripts/KeyEvent.cs
+++ Assets/Scripts/KeyEvent.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 public class KeyEvent : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameStatus key;
-  
+    private GameStatus key;
+
+    void Start()
+    {
+        key = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameStatus>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             key.keyCount++;
             Destroy(this.gameObject);
-          
+
         }
     }
 }
