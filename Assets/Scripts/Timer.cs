52a63eac96f9dabcd9a78f9289b13511c5ef3f50
Chi-Sheng Yang <chisheng@usc.edu>
2021-04-19 14:46:09 -0700

    timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        end = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (end)
+            return;
+        float t = Time.time - startTime;
+        string min = ((int)t / 60).ToString();
+        string sec = ((int)t % 60).ToString("f2");
+
+        timerText.text = min + ":" + sec;
+    }
+
+    public void setEnd()
+    {
+        end = true;
+    }
+
+    public void setResume()
+    {
+        end = false;
+    }
+}
