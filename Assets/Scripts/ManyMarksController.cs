a1c9dd5a291947f7dc1592928f6cb4a29c70b852
terdo <zhang_ruitao@hotmail.com>
2021-04-29 01:24:06 -0700

    [Add] Many coin marks

---
+++ Assets/Scripts/ManyMarksController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManyMarksController : MonoBehaviour
+{
+    public string targetTag = "star";
+    public GameObject targetPrefab;
+    private GameObject[] targets;
+    private bool hasTarget = false;
+    private bool hasCreated = false;
+    private void Update()
+    {
+        if (!hasTarget)
+        {
+            targets = GameObject.FindGameObjectsWithTag(targetTag);
+            hasCreated = false;
+        }
+        if (targets.Length != 0)
+        {
+            hasTarget = true;
+            if (!hasCreated)
+            {
+                foreach (GameObject it in targets)
+                {
+                    GameObject instance = GameObject.Instantiate(targetPrefab, transform);
+                    instance.GetComponent<TargetMark>().target = it;
+                }
+                hasCreated = true;
+            }
+        }
+        else hasTarget = false;
+    }
+}
---
+++ Assets/Scripts/TargetMark.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMark : MonoBehaviour
+{
+    public GameObject target;
+    private Transform targetTransform;
+    private Transform playerTransform;
+
+    public float x0 = 4.88f;
+    public float y0 = 2.63f;
+    public float radius = 10;
+    private float k;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        RectTransform canv = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        y0 = (canv.sizeDelta.y - 60) * canv.localScale.y / playerTransform.localScale.y / 2;
+        x0 = (canv.sizeDelta.x - 60) * canv.localScale.x / playerTransform.localScale.x / 2;
+        k = y0 / x0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (target)
+        {
+            targetTransform = target.transform;
+            float distance = (targetTransform.position - playerTransform.position).magnitude;
+            if (distance > radius)
+            {
+                SetMarkAccordingTo(targetTransform.position, playerTransform.position);
+            }
+            else transform.localPosition = new Vector3(0, 0, -100);
+        }
+        else Destroy(gameObject);
+
+    }
+
+    private void SetMarkAccordingTo(Vector2 target, Vector2 player)
+    {
+        float f1 = k * (player.x - target.x) + target.y - player.y;
+        float f2 = k * (target.x - player.x) + target.y - player.y;
+        float outX = 0;
+        float outY = 0;
+        if (f1 > 0 && f2 > 0)
+        {
+            outX = (target.x - player.x) * y0 / (target.y - player.y);
+            outY = y0;
+        }
+        else if (f1 < 0 && f2 < 0)
+        {
+            outX = (player.x - target.x) * y0 / (target.y - player.y);
+            outY = -y0;
+        }
+        else if (f1 > 0 && f2 < 0)
+        {
+            outY = (target.y - player.y) * x0 / (player.x - target.x);
+            outX = -x0;
+        }
+        else if (f1 < 0 && f2 > 0)
+        {
+            outY = (target.y - player.y) * x0 / (target.x - player.x);
+            outX = x0;
+        }
+        else if (f1 == 0)
+        {
+            if (f2 > 0) (outX, outY) = (x0, y0);
+            else (outX, outY) = (-x0, -y0);
+        }
+        else if (f2 == 0)
+        {
+            if (f1 > 0) (outX, outY) = (-x0, y0);
+            else (outX, outY) = (x0, -y0);
+        }
+        transform.localPosition = new Vector2(outX, outY);
+    }
+}
